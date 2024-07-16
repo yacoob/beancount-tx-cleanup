16ac33aa088eba7cd1a8e8db0baa1168a3c1b8df
Jakub Turski <yacoob@ftml.net>
2024-07-16 14:37:26 +0100

    hello world!
    This is a continuation of the beancount playground.
    https://git.normally.works/yacoob/finance-playground
    
    From now on, this repository contains day to day finance data plus the
    code and config necessary to maintain it. It does NOT contain historical
    data exported from Moneydance or the code that converted it to beancount
    format.
    
    pythonpath + wslview

---
+++ beancount_tx_cleanup/cleaner.py
@@ -0,0 +1,130 @@
+"""An helper function that cleans up the payee field and adds discovered information to entry's metadata."""
+
+import datetime
+import re
+from dataclasses import InitVar, field
+from typing import Callable, Iterable, Optional
+
+from pydantic.dataclasses import dataclass
+
+TAG_DESTINATION = '__TAG'
+CLEANUP = '__CLEANUP'
+AGES_AGO = datetime.date(1900, 1, 1)
+
+
+# Each Extractor describes a transformation performed on the transaction's payee.
+# An Extractor object gets initialized with:
+# ```
+#     match: RE to match payee against
+#     replacement: string or lambda taking a re.Match object (see: re.sub)
+#                  to replace matched part of the payee field with (default='')
+#     value: string to use as a value of the metadata, fed to Match.expand() (default='\1')
+#     transformer: lambda to apply to the value string (default: lambda s: s)
+#     translation: optional lookup table to look up value.lower() (default={})
+# ```
+@dataclass
+class Extractor:
+    """Single extractor definition."""
+
+    match: InitVar[str]
+    regexp: re.Pattern = field(init=False)
+    replacement: str | Callable[[re.Match], str] = field(kw_only=True, default='')
+    value: str = field(kw_only=True, default=r'\1')
+    transformer: Callable[[str], str] = field(kw_only=True, default=lambda s: s)
+    translation: dict[str, str] = field(kw_only=True, default_factory=dict)
+    last_used: datetime.date = field(kw_only=True, default=AGES_AGO)
+
+    def __post_init__(self, match):  # noqa: D105
+        self.regexp = re.compile(match)
+
+
+# Each entry in Extractors dict looks like this:
+# `name: (ExtractorA, ExtractorB, ...)`
+#
+# If an extractor successfully applies, metadata gets a new entry of `name: value`.
+# If the `name` is `TAG_DESTINATION`, a tag is added instead of a metadata entry.
+# If the `name` is `CLEANUP`, only the replacement is performed, and no value is extracted.
+#
+# NOTE: The ordering of `name`s in Extractors and Extractor objects themselves on the list
+# is important; some REs use anchors that won't match anymore after a preceeding extractor
+# modifies the payee string.
+#
+E = Extractor
+Extractors = dict[str, tuple[Extractor, ...]]
+
+
+def TxnPayeeCleanup(
+    txn,
+    extractors: Optional[Extractors] = None,
+    preserveOriginalIn: Optional[str] = None,
+):
+    """Extract extra information from the payee field of the Transaction."""
+    if extractors is None:
+        extractors = {}
+    # get current txn fields
+    original_payee = payee = txn.payee
+    tags = set(txn.tags)
+    meta = txn.meta.copy()
+    # remove selected stars from the description, AIB loves to sprinkle them over
+    payee = payee.strip('*').replace(' *', ' ').replace('* ', ' ').strip()
+    # apply extractors to the payee field
+    for name, list_of_extractors in extractors.items():
+        for extractor in list_of_extractors:
+            if m := extractor.regexp.search(payee):
+                # record the most recent timestamp this extractor applied to:
+                if txn.date > extractor.last_used:
+                    extractor.last_used = txn.date
+                # stash the original payee for future record
+                if preserveOriginalIn and preserveOriginalIn not in meta:
+                    meta[preserveOriginalIn] = original_payee
+                # replace the match in payee
+                payee = re.sub(extractor.regexp, extractor.replacement, payee).strip()
+                # Was this a cleanup extractor?
+                if name is CLEANUP:
+                    continue
+                # extract the value
+                value = m.expand(extractor.value).strip()
+                # apply the transformation
+                value = extractor.transformer(value)
+                # run the value through a lookup table
+                value = extractor.translation.get(value.lower(), value)
+                # assign the value to tag set or metadata entry
+                if name is TAG_DESTINATION:
+                    tags.add(value)
+                else:
+                    meta[name] = value
+    return txn._replace(payee=payee, tags=tags, meta=dict(sorted(meta.items())))
+
+
+@dataclass
+class ExtractorUsage:
+    """Structure for usage reporting of a single extractor."""
+
+    date: datetime.date
+    rulename: str
+
+    def __str__(self) -> str:  # noqa: D105
+        return f'{self.date.strftime("%Y-%m-%d")}: {self.rulename}'
+
+
+class ExtractorsUsage(list[ExtractorUsage]):
+    """Structure for usage reporting of a whole extractor set."""
+
+    def __init__(self, iterable: Iterable):  # noqa: D107
+        super().__init__(ExtractorUsage(*x) for x in iterable)
+
+    def __str__(self) -> str:  # noqa: D105
+        return '\n'.join(str(u) for u in self)
+
+
+def extractorsUsage(extractors: Extractors) -> ExtractorsUsage:
+    """For every extractor in the set, reports what was the date of the most recent transaction processed by it."""
+    return ExtractorsUsage(
+        sorted(
+            [
+                (e.last_used, f'{name} - {e.regexp}')
+                for name, es in extractors.items()
+                for e in es
+            ],
+        ),
+    )
---
+++ beancount_tx_cleanup/cleaner_test.py
@@ -0,0 +1,143 @@
+"""Tests for the transaction cleaner."""
+
+import datetime
+from dataclasses import field
+
+import pytest
+from beancount_aib.helpers import Tx
+from beancount_tx_cleanup.cleaner import (
+    CLEANUP,
+    TAG_DESTINATION,
+    E,
+    Extractors,
+    TxnPayeeCleanup,
+    extractorsUsage,
+)
+from pydantic.dataclasses import dataclass
+
+
+@dataclass
+class CleanerScenario:
+    """A dataclass describing a test scenario for cleaner.
+
+    TxnPayeeCleanup should turn
+      Tx(payee=input)
+    into
+      Tx(payee, tags, meta)
+    """
+
+    input: str
+    payee: str
+    tags: set = field(default_factory=set, kw_only=True)
+    meta: dict = field(default_factory=dict, kw_only=True)
+
+
+CS = CleanerScenario
+
+
+class BasicExtractorTest:
+    """Cleaner functionality tests."""
+
+    date = datetime.date(2071, 3, 14)
+
+    @pytest.fixture()
+    def extractors(self):
+        """Provide a set of base extractors, which can be modified as needed in each test."""
+        return Extractors({
+            'id': (
+                # extract '^XY1234', add id=XY1234 to metadata
+                E(r'(?i)^(XY9\d+)'),
+                # extract '^ID1234', lowercase it, add id=id1234 to metadata
+                E(r'(?i)^(ID\d+)', transformer=lambda s: s.lower()),
+                # match '^GTS1234', add id=v-1234 to metadata, replace 'GTS1234' with '4321'
+                E(r'(?i)^GTS(\d+)', value=r'v-\1', replacement=lambda m: m.group(1)[::-1]),
+            ),
+            TAG_DESTINATION: (
+                # match '12.34 ABC@ 0.13 $'', extract abc, run it through the lookup table, no replacement
+                E(r' [\d.]+ ([A-Z]{3})@ [\d.]+ *$', translation={'jpy': '¥'}, replacement=r'\g<0>'),
+            ),
+            CLEANUP: (
+                # match '@ 0.13$', replace with ' (0.13 each)', no extraction
+                E(r'@ ([\d.]+)$', replacement=r' (\1 each)'),
+            ),
+        })  # fmt: skip
+
+
+class TestCleanerFunctionality(BasicExtractorTest):
+    """These tests exercise the basic functionality of TxnPayeeCleanup.
+
+    To fully test the custom rules defined by the importer you should also have:
+    - a beancount.ingest.regression_pytest based on actual bank CSVs
+    - a test that checks if all extractors are tested by the regression tests
+    """
+
+    CLEANER_SCENARIOS = (
+        # no extractor matches this payee
+        CS('*Fredrikson*and Sons *Ltd.*', 'Fredrikson*and Sons Ltd.'),
+        # straightforward extraction
+        CS('XY90210 Happy Days', 'Happy Days', meta={'id': 'XY90210'}),
+        # extraction plus a lambda transformer
+        CS('ID1234 *standing order', 'standing order', meta={'id': 'id1234'}),
+        # extraction with a custom value plus a lambda replacement
+        CS('GTS98765 regular saver', '56789 regular saver', meta={'id': 'v-98765'}),
+        # extraction to a tag, a lookup table then a cleanup with a string replacement
+        CS('AirSide Coffee 12.30 JPY@ 0.13  ', 'AirSide Coffee 12.30 JPY (0.13 each)', tags={'¥'}),
+    )  # fmt: skip
+
+    @pytest.mark.parametrize('scenario', CLEANER_SCENARIOS)
+    def test_cleaning(self, extractors, scenario):
+        """Test different scenarios from CLEANER_SCENARIOS."""
+        tx = Tx(self.date, scenario.input)
+        clean_tx = Tx(
+            self.date,
+            scenario.payee,
+            tags=scenario.tags,
+            meta=scenario.meta,
+        )
+        assert clean_tx == TxnPayeeCleanup(tx, extractors, preserveOriginalIn=None)
+
+    def test_empty_extractors(self):
+        """Empty extractor list should result in no changes other than star removal."""
+        tx = Tx(self.date, 'shai hulud extract')
+        assert tx == TxnPayeeCleanup(tx, None)
+        tx = Tx(self.date, '*shai hulud extract')
+        clean_tx = Tx(self.date, 'shai hulud extract')
+        assert clean_tx == TxnPayeeCleanup(tx, {})
+
+    def test_save_original_payee(self, extractors):
+        """TxnPayeeCleanup will optionally preserve the tx's original payee."""
+        p = 'ID19283 *standing order'
+        tx = Tx(self.date, p)
+        clean_tx = Tx(
+            self.date,
+            'standing order',
+            meta={'id': 'id19283', 'previously': p},
+        )
+        assert clean_tx == TxnPayeeCleanup(
+            tx,
+            extractors,
+            preserveOriginalIn='previously',
+        )
+
+    def test_extractor_order_swap(self, extractors):
+        """The ordering of extractors is important; in this test we swap the order of applications of  __CLEANUP and TAG_DESTINATION. As a result, payee gets modified in a similar way, but no tag is extracted."""
+        e = list(extractors.items())
+        reordered_extractors: Extractors = dict([e[0], e[2], e[1]])
+        tx = Tx(self.date, self.CLEANER_SCENARIOS[4].input)
+        clean_tx = Tx(self.date, self.CLEANER_SCENARIOS[4].payee)
+        assert clean_tx == TxnPayeeCleanup(tx, reordered_extractors)
+
+
+class TestExtractorsUsageReporting(BasicExtractorTest):  # noqa: D101
+    def test_reporting(self, extractors):
+        """TxnPayeeCleanup records date of the most recent tx each extractor matched."""
+        _ = TxnPayeeCleanup(
+            Tx(self.date, 'ID29381 Grooveshark subscription'),
+            extractors,
+        )
+        expected_usage = r"""1900-01-01: __CLEANUP - re.compile('@ ([\\d.]+)$')
+1900-01-01: __TAG - re.compile(' [\\d.]+ ([A-Z]{3})@ [\\d.]+ *$')
+1900-01-01: id - re.compile('(?i)^(XY9\\d+)', re.IGNORECASE)
+1900-01-01: id - re.compile('(?i)^GTS(\\d+)', re.IGNORECASE)
+2071-03-14: id - re.compile('(?i)^(ID\\d+)', re.IGNORECASE)"""
+        assert expected_usage == str(extractorsUsage(extractors))
